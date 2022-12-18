9395e13dddfd0874b8e34265391c9d535ffb00f0
Victor Hugo <victorhugofny@gmail.com>
2022-12-18 14:04:49 -0300

    novas rotas

---
+++ codigosAux.js
@@ -0,0 +1,9 @@
+//Criar tabelas
+//db.run("CREATE TABLE problems (id number, problem string, description string, correctWords string, randomWords string, xp number, level string, lock string, completed string)");
+// db.run("CREATE TABLE score (id_usuario number, id number ,totalXp number)");
+// db.run("CREATE TABLE desafio (id number, id_problem number)");
+
+//inserir dados
+//db.run("INSERT INTO problems (id, problem, description, correctWords, randomWords, xp, level, lock, completed) VALUES (1, '??(3 === 3) {console.log(´acertou´)}', 'O numero 3 é igual a 3?',  'if, for', 'for, while, if, break, i++, const, forEach', 16, 'easy' , 'true', 'true')");
+// db.run("INSERT INTO score (id_usuario, id, totalXp) VALUES (1, 1, 0)");
+// db.run("INSERT INTO desafio (id, id_problem) VALUES (1, 1)");
--- index.js
+++ index.js
@@ -1,21 +1,131 @@
 const express = require('express');
 const server = express();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database("./db.sqlite");
 
-const port = process.env.PORT || 3000
+let id_problemas = 1;
+let id_score = 1;
+let id_desafio = 1;
+
+const port = process.env.PORT || 3000;
 server.use(express.json());
 
-//Listar pessoas
-server.get("/perguntas",(req, res)=>{
-    res.sendFile(__dirname + "/dados.json");
+let listaTabela = [];
+
+//usando promisse para busca assincrona
+function buscarTabela(tabela){
+    return new Promise((resolve, reject) => {
+
+    db.all(`SELECT * FROM ${tabela}`, (err, rows) => {
+        if(rows){
+            rows.forEach((row) =>{
+                if(tabela == "problems"){
+                    listaTabela.push({
+                        "id": row.id,
+                        "problem": row.problem,
+                        "description": row.description,
+                        "correctWords": row.correctWords,
+                        "randomWords": row.randomWords,
+                        "xp": row.xp,
+                        "level": row.level,
+                        "lock": row.lock,
+                        "completed": row.completed
+                    })
+                } 
+                else if(tabela == "desafio"){
+                    listaTabela.push({
+                        "id": row.id,
+                        "id_problem": row.id
+                    })
+                }
+                else if(tabela == "score"){
+                    listaTabela.push({
+                        "id": row.id,
+                        "id_usuario": row.id_usuario,
+                        "totalXp": row.totalXp
+                    })
+                }
+            })
+        }else{
+            reject("Vazio")
+        }
+    })
+    resolve(listaTabela);
+    })
+}
+
+server.get("/problemas",(req, res)=>{
+    buscarTabela("problems").then((data) => res.json(listaTabela))
 })
 
+server.get("/score",(req, res)=>{
+    buscarTabela("score").then((data) => res.json(listaTabela))
+})
+
+server.get("/desafio",(req, res)=>{
+    buscarTabela("desafio").then((data) => res.json(listaTabela))
+})
+
+//pagina html
 server.get("/",(req, res)=>{
     res.sendFile(__dirname + "/index.html");
 })
 
-// server.post('/perguntas:enviar', async (req, res)=>{  
-// })        
+server.post('/CriarProblema', (req, res)=>{
+    const 
+    {   
+        problem, 
+        description, 
+        correctWords, 
+        randomWords, 
+        xp, 
+        level,
+        lock,
+        completed
+    } = req.body;
+
+    id_problemas++;
+
+     db.run(`INSERT INTO problems (id, problem, description, correctWords, randomWords, xp, level, lock, completed) 
+        VALUES (${id_problemas},'${problem}' , '${description}' , '${correctWords}' , '${randomWords}' , ${xp} , '${level}', '${lock}', '${completed}')`);
+
+    buscarTabela("problems").then((data) => res.json(listaTabela))
+});
+
+server.post('/CriarScore', (req, res)=>{
+    const {id_usuario, totalXp} = req.body;
+    id_score++;
+    db.run(`INSERT INTO score (id_usuario, id, totalXp) VALUES (${id_usuario},${id_score}, ${totalXp})`);
+    buscarTabela("score").then((data) => res.json(listaTabela))
+});
+
+//vincular um problema a um desafio pelo id;
+server.post('/CriarDesafio', (req, res)=>{
+    const {id_problem} = req.body;
+    id_desafio++;
+    db.run(`INSERT INTO desafio (id, id_problem) VALUES (${id_desafio},${id_problem})`);
+    buscarTabela("desafio").then((data) => res.json(listaTabela))
+});
+
+//atualizando o score
+server.put('/score/:index',(req,res)=>{
+    const {index} = req.params;
+    const {totalXp} = req.body;
+
+    db.run(`UPDATE score SET totalXp = ${totalXp} WHERE id_usuario = ${index}`);
+    buscarTabela("score").then((data) => res.json(listaTabela))
+});
+
+//delete um problema
+server.delete('/problema/:index',(req,res)=>{
+    const {index} = req.params;
+    db.run(`DELETE FROM problems WHERE id = ${index}`);
+
+    buscarTabela("problems").then((data) => res.json("deletado"))
+})
 
 server.listen(port,()=>{
     console.log("servidor rodando na porta "+ port);
 });
+
+
